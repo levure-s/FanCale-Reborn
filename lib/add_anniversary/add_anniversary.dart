8f71f9662e937a70361175326e3aa939bd36dcd5
levure-s <levure.sauvage@gmail.com>
2025-04-06 11:58:56 +0900

    年を選択できるように変更

--- lib/add_anniversary/add_anniversary.dart
+++ lib/add_anniversary/add_anniversary.dart
@@ -9,12 +9,55 @@ class AddAnniversary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController controller = TextEditingController();
+    final TextEditingController yearController =
+        TextEditingController(text: selectedDay?.year.toString());
 
     return ChangeNotifierProvider(
       create: (_) => AddAnniversaryModel(), // 変更したモデルを使う
       child: AlertDialog(
         title: const Text('記念日を入力してください'), // 記念日メモに変更
-        content: TextField(controller: controller),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(controller: controller),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: yearController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: '年',
+                      hintText: '手入力 or ▼ボタンで選択',
+                    ),
+                  ),
+                ),
+                PopupMenuButton<int>(
+                  icon: const Icon(Icons.arrow_drop_down),
+                  onSelected: (value) {
+                    yearController.text = value.toString();
+                  },
+                  itemBuilder: (context) {
+                    return List.generate(30, (index) {
+                      final year = DateTime.now().year - index;
+                      return PopupMenuItem(
+                        value: year,
+                        child: Text('$year年'),
+                      );
+                    });
+                  },
+                ),
+                TextButton(
+                  onPressed: () {
+                    yearController.text = ''; // 年を空にする
+                  },
+                  child: const Text('年を指定しない'),
+                ),
+              ],
+            )
+          ],
+        ),
         actions: [
           TextButton(
               onPressed: () {
@@ -25,8 +68,8 @@ class AddAnniversary extends StatelessWidget {
             return TextButton(
                 onPressed: () async {
                   try {
-                    await model.addAnniversary(
-                        selectedDay, controller.text); // 変更したメソッド名
+                    await model.addAnniversary(selectedDay, controller.text,
+                        yearController.text); // 変更したメソッド名
                   } catch (e) {
                     final snackBar = SnackBar(
                         backgroundColor: Colors.red,
--- lib/add_anniversary/add_anniversary_model.dart
+++ lib/add_anniversary/add_anniversary_model.dart
@@ -3,7 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AddAnniversaryModel extends ChangeNotifier {
-  Future addAnniversary(DateTime? selectedDay, String? text) async {
+  Future addAnniversary(
+      DateTime? selectedDay, String? text, String yearText) async {
     if (selectedDay == null) {
       throw '日付を選択してください';
     }
@@ -11,9 +12,17 @@ class AddAnniversaryModel extends ChangeNotifier {
       throw 'なんの記念日か入力してください';
     }
 
+    final year = yearText.isNotEmpty ? int.tryParse(yearText) : 9999;
+
+    if (year == null || year < 0) {
+      throw '無効な年が入力されました';
+    }
+
+    final updatedDate = DateTime(year, selectedDay.month, selectedDay.day);
+
     final uid = FirebaseAuth.instance.currentUser?.uid;
     await FirebaseFirestore.instance.collection('anniversaries').add({
-      'date': Timestamp.fromDate(selectedDay), // 日付をTimestampとして保存
+      'date': Timestamp.fromDate(updatedDate), // 日付をTimestampとして保存
       'title': text,
       'uid': uid,
       'memo': [],
